5fe8b3ed5c40f27c12497f822a6048af1917e8ca
danielghost20 <danielramosmartinez72@gmail.com>
2023-11-06 20:41:29 -0600

    fix: ad typescript to the firebase-admin file

--- src/app/firebase-admin.ts
+++ src/app/firebase-admin.ts
@@ -6,9 +6,9 @@ let app;
 if (!admin.apps.length) {
     app = admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY
+            projectId: process.env.FIREBASE_PROJECT_ID as string,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY as string
         })
     })
 }
@@ -16,9 +16,9 @@ if (!admin.apps.length) {
 
 const admindOb = initFirestore({
     credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY
+        projectId: process.env.FIREBASE_PROJECT_ID as string,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY as string
     })
 })
 
