f7df332cc7031a4ab02d09244494f6cc5974d645
danielghost20 <danielramosmartinez72@gmail.com>
2023-11-09 13:53:29 -0600

    fix: add regular expresion to transform api key on firebase-admin.ts

--- src/app/firebase-admin.ts
+++ src/app/firebase-admin.ts
@@ -8,7 +8,7 @@ if (!admin.apps.length) {
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID as string,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY as string
+            privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
         })
     })
 }
@@ -18,7 +18,7 @@ const admindOb = initFirestore({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID as string,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY as string
+        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
     })
 })
 
